6de58cd7f1205b2656cb1f2f2b625f6f97b4be9a
anthobldr <anthony.lutard33@gmail.com>
2026-09-18 17:36:50 +0200

    feat(footer): add responsive style

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,10 +2,10 @@ import "./css/footer.css"
 
 export default function Footer(){
     return (
-        <footer className="py-lg-3 px-lg-5">
-            <div className="d-flex justify-content-between">
+        <footer className="py-3 px-lg-5">
+            <div className="d-flex flex-column flex-lg-row justify-content-lg-between text-center text-lg-start">
                 <span>© 2026 Minepiece - Tous droits réservé.</span>
-                <ul className="list-unstyled d-flex gap-lg-4">
+                <ul className="list-unstyled d-flex justify-content-center justify-content-lg-start gap-3 gap-lg-4">
                     <li>Mentions légales</li>
                     <li>Politique de confidentialités</li>
                 </ul>
