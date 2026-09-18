19aa579c15f768acb0ab939bd2729d60db597dc6
anthobldr <anthony.lutard33@gmail.com>
2026-09-18 19:40:18 +0200

    feat(security): add mc-head in next/image security

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mc-heads.net",
+        pathname: "/avatar/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
