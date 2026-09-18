8a99f931b234fe2531aaca58c830962ab11a8c2e
anthobldr <anthony.lutard33@gmail.com>
2026-09-18 17:31:39 +0200

    feat(navbar): add responsive navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,17 +8,19 @@ export default function Navbar(){
             <div className="d-flex justify-content-lg-between align-items-center px-lg-4 py-lg-3 py-2 headerTop">
                 <Image src={"/images/logo-gif.gif"} alt="Logo du serveur MinePiece animé." width={60} height={50} />
                 <h1>MinePiece</h1>
-                <div className="d-none d-lg-flex">
-                    <BtnPrimary text="Télécharger le launcher" />
+                <div className="ms-auto ms-lg-0">
+                    <div className="d-none d-lg-block">
+                        <BtnPrimary text="Télécharger le launcher" />
+                    </div>
+                    <button className="navbar-toggler d-block d-lg-none ms-auto me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                 </div>
             </div>
             <nav className="navbar navbar-expand-lg px-lg-4 py-lg-2">
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav d-flex gap-lg-4">
+                        <ul className="navbar-nav d-flex text-center text-lg-start gap-3 gap-lg-4">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Accueil</a>
                             </li>
